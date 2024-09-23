cd58fa41b6725c39efbfae1cb55a621b95ba240f
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2024-09-23 14:22:06 +0900

    Update bencmark script

--- benchmark.py
+++ benchmark.py
@@ -4,19 +4,29 @@ import numpy as np
 times_qiskit = []
 times_pennylane = []
 times_qulacs = []
-for j in range(2, 17):
-    for package in [psr.psr_qiskit, psr.psr_pennylane, psr.psr_qulacs]:
-        start = time.time()
-        package(j)
-        end = time.time()
-        exe_time = end-start
-        if package == psr.psr_qiskit:
-            times_qiskit.append(exe_time)
-        elif package == psr.psr_pennylane:
-            times_pennylane.append(exe_time)
-        else:
-            times_qulacs.append(exe_time)
-
-np.savetxt("./times/qiskit.txt", times_qiskit)
-np.savetxt("./times/pennylane.txt", times_pennylane)
-np.savetxt("./times/qulacs.txt", times_qulacs)
+for k in range(0, 5):
+    print("Times: ", k)
+    time_qiskit = []
+    time_pennylane = []
+    time_qulacs = []
+    for j in range(2, 17):
+        for package in [psr.psr_qiskit, psr.psr_pennylane, psr.psr_qulacs]:
+            start = time.time()
+            package(j)
+            end = time.time()
+            exe_time = end-start
+            if package == psr.psr_qiskit:
+                time_qiskit.append(exe_time)
+            elif package == psr.psr_pennylane:
+                time_pennylane.append(exe_time)
+            else:
+                time_qulacs.append(exe_time)
+    times_qiskit.append(time_qiskit)
+    times_pennylane.append(time_pennylane)
+    times_qulacs.append(time_qulacs)
+np.savetxt("./times/qiskit.txt", np.mean(times_qiskit))
+np.savetxt("./times/pennylane.txt", np.mean(times_pennylane))
+np.savetxt("./times/qulacs.txt", np.mean(times_qulacs))
+np.savetxt("./times/qiskit_std.txt", np.std(times_qiskit))
+np.savetxt("./times/pennylane_std.txt", np.std(times_pennylane))
+np.savetxt("./times/qulacs_std.txt", np.std(times_qulacs))
